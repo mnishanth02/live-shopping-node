0e0a927c858fe17c5f87cc49cbcc9ddff3cebf12
nishanth Murugan <nishanth.murugan@gmail.comn>
2020-07-12 16:39:24 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,19 +2,12 @@ const express = require("express");
 require('./db/mongoose');
 const cors = require("cors");
 const path = require("path");
-const bodyParser = require("body-parser");
-// const passport = require('passport')
-// const passportMiddleWare = require('./middleware/passport')
 
 const userRoutes = require("./router/userRouter");
 const ownerRoutes = require("./router/shopRouter");
 
 const app = express();
 app.use(cors());
-// app.use(bodyParser.json({ limit: '50mb', strict: false }));
-// app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
-
-
 
 app.use(express.json());
 app.use("/src/images", express.static(path.join("src/images")));
@@ -46,7 +39,6 @@ app.use("/api/shop", ownerRoutes);
 
 app.all('*', (req, res, next) => {
 
-  // next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 
   res.status(404).json({
     status: 'fail',
